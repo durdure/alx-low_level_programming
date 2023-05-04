c96449c823f8681a5249ba7ff95a01f2086fab4e
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-05-04 04:59:17 -0700

    :sparkles: Good :sparkles:

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef _BIT_MANIP_
 #define _BIT_MANIP_
 
-int _strlen(const char *s);
+int length_of_string(const char *s);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
