e82d5c3f360467b80f7cafa6cfd9a648b480f9c8
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-05-04 09:49:53 +0300

    :spackles: Good :spackles:

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@ int length_of_string(const char *s);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
-void _divide(unsigned long int n);
+void _div(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
