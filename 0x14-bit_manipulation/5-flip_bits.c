abc5d57ee1f5720e9687ebc1a69a931f215b2a40
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-05-04 12:01:07 +0300

    :sparkles: Good :sparkles:

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * flip_bits - ...
+  * @n: ...
+  * @m: ...
+  *
+  * Return: ...
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int cover = 0, val = 0;
+
+	mask = n ^ m;
+	while (cover)
+	{
+		if (cover & 1)
+			val++;
+
+		cover >>= 1;
+	}
+
+	return (val);
+}
