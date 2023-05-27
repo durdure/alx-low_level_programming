2d878707fa333b9ee15f60e27591bf9a475d95da
Duresa EShetu <duresaeshetu2001@gmail.com>
2023-05-27 18:02:15 +0300

    :sparkles:

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
+	buff = malloc(sizeof(char) * (letters));
 	if (!buff)
 		return (0);
 
