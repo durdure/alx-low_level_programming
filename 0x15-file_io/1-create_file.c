0065439ea2cae6130ea72ab79c92e312bb8989ce
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-07-10 20:44:59 +0300

    cool

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_da;
-	int number_let;
-	int row_wr;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file_da == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (number_let = 0; text_content[number_let]; number_let++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(file_d, text_content, number_let);
+	rwr = write(fd, text_content, nletters);
 
-	if (row_wr == -1)
+	if (rwr == -1)
 		return (-1);
 
-	close(file_d);
+	close(fd);
 
 	return (1);
 }
